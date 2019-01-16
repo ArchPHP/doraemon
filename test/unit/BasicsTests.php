8ed3f0814eb8675e2122e7695fddf4dddfd5dfa8
Yumin Gui <guiyumin@outlook.com>
2019-01-15 23:03:57 -0800

    ArrayHelper chunk, optimize test cases 2

--- test/unit/BasicsTests.php
+++ test/unit/BasicsTests.php
@@ -10,13 +10,11 @@ class BasicsTests extends TestCase
     public function testChunk()
     {
         $arr1 = ['a', 'b' => 'b', 'c', 'd'];
-        $result1 = ArrayHelper::chunk($arr1, 2);
-        $r1 =$result1 === [['a','b'],['c','d']];
-
-        $arr2 = ['a', 'b', 'c', 'd'=>'d'];
-        $result2 = ArrayHelper::chunk($arr2,3,true);
-        $r2 = $result2 === [['a','b','c'],['d'=>'d']];
+        $r1 = ArrayHelper::chunk($arr1, 2) === [['a','b'],['c','d']];
         
+        $arr2 = ['a', 'b', 'c', 'd'=>'d'];
+        $r2 = ArrayHelper::chunk($arr2,3,true) === [['a','b','c'],['d'=>'d']];
+    
         $r = $r1 && $r2;
         $this->assertTrue($r);
     }
