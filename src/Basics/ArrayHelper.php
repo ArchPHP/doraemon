7bcaa1e74e8e523d847d9b99dc188b35898894cb
Yumin Gui <guiyumin@outlook.com>
2019-01-20 23:45:06 -0800

    copyright information added

--- src/Basics/ArrayHelper.php
+++ src/Basics/ArrayHelper.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * the ArchPHP Doraemon project
+ *
+ * @author      Yumin Gui <guiyumin@gmail.com>
+ * @copyright   (c) Yumin Gui
+ * @link        https://github.com/archphp/doraemon
+ * @license     MIT
+ */
 
 namespace ArchPHP\Doraemon\Basics;
 
